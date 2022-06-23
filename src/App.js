c1f0211cb98ee4bb4c2b5ed8087a66adad55f4ce
Cruz Rocha <juancruzrocha@live.com>
2022-06-23 18:38:26 -0300

    i just finished my weather app

--- src/App.js
+++ src/App.js
@@ -1,81 +1,82 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Cards from './components/Cards.jsx';
 import Nav from './components/nav';
 import { Route } from "react-router-dom";
 import About from './components/About.jsx';
-import Ciudad  from './components/Ciudad.jsx';
+import Ciudad from './components/Ciudad.jsx';
 
 
 //import Card from './components/Card.jsx';
 // acá en app.js es donde a los componentes coomo nav le vamos a pasar las propiedades que queremos que utilice cada componente
 function App() {
-  
-   const [cities , setCities]= useState([]);  //estoy creando un estado
 
-   const apiKey = '4ae2636d8dfbdc3044bede63951a019b'; //Este es el codigo para hacer la peticion a la API 
-  
-   function onClose(id) {
-         setCities( oldCities => oldCities.filter( c => c.id !== id)); // me quedo con todos los que son distintos a ese id
-   }
-   
+  const [cities, setCities] = useState([]);  //estoy creando un estado
+
+  const apiKey = '4ae2636d8dfbdc3044bede63951a019b'; //Este es el codigo para hacer la peticion a la API 
+
+  function onClose(id) {
+    setCities(oldCities => oldCities.filter(c => c.id !== id)); // me quedo con todos los que son distintos a ese id
+  }
+
   function onSearch(ciudad) {
-    
+
     // Siempre que usamos fetch a una API que responde en JSON debemos hacer los mismos pasos:
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)   // ⬅️ 1) llamada a la API, el resultado es una Promise
-    .then(response => response.json()) // ⬅️ 2) cuando la petición finalice, transformamos la respuesta a JSON (response.json() también es una Promise)
-    .then(json => {
-      if (json  !== undefined) {       // si existe entonces...
-        
-        const ciudad = {               //creamos una variable ciudad que es donde guardamos toda la data que nos traemos de la API
-          min:     Math.round(json.main.temp_min),
-          max:     Math.round(json.main.temp_max),
-          img:     json.weather[0].icon,
-          wind:    json.wind.speed,
-          temp:    json.main.temp,
-          name:    json.name,
-          weather: json.weather[0].main,
-          clouds:  json.clouds.all,
-          latitud: json.coord.lat,
-          longitud:json.coord.lon,
-          id:      json.id
+      .then(response => response.json()) // ⬅️ 2) cuando la petición finalice, transformamos la respuesta a JSON (response.json() también es una Promise)
+      .then(json => {
+        if (json !== undefined) {       // si existe entonces...
+
+          const ciudad = {               //creamos una variable ciudad que es donde guardamos toda la data que nos traemos de la API
+            min: Math.round(json.main.temp_min),
+            max: Math.round(json.main.temp_max),
+            img: json.weather[0].icon,
+            wind: json.wind.speed,
+            temp: json.main.temp,
+            name: json.name,
+            weather: json.weather[0].main,
+            clouds: json.clouds.all,
+            latitud: json.coord.lat,
+            longitud: json.coord.lon,
+            id: json.id
+          }
+
+          //console.log(ciudad)                  // ⬅️ 3) aquí ya tenemos la respuesta en formato objeto
+
+          setCities(oldCities => [...oldCities, ciudad]);   // hago una copia de oldCities y le agrego   ad esto me da un arreglo nuevo
+        } else {
+          alert('Ciudad no encontrada.');
         }
-        
-        //console.log(ciudad)                  // ⬅️ 3) aquí ya tenemos la respuesta en formato objeto
-        
-        setCities( oldCities => [...oldCities, ciudad]);   // hago una copia de oldCities y le agrego   ad esto me da un arreglo nuevo
-      } else {
-        alert('Ciudad no encontrada.');
-      }
-    })
-   }
+      })
+  }
 
 
   function onFilter(ciudadId) {     // esta function recibe ciudadId y cities (un arreglo de objetos que tiene todas las ciudades). Nos va a devolver la unica ciudad que tenga el mismo id en ciudadId y cities
-    let ciudad = cities.filter( c => c.id === parseInt(ciudadId));   // usso  el parseInt porque c.id es un string y lo quiero convertir a numero
+    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));  // uso  el parseInt porque c.id es un string y lo quiero convertir a numero
     if (ciudad.length > 0) {
       return ciudad[0]
     } else return null;
   }
-   
+
   return (
     <div className="App" id="App">
-      
-      
-       <Route path ='/' render = { () =>   <Nav onSearch={onSearch}/>}  />   {/* le paso la funcion pero no la invocacion */}
-       <Route exact path = '/' render = { () => <Cards  
-        cities={cities} 
+
+
+      <Route path='/' render={() => <Nav onSearch={onSearch} />} />   {/* le paso la funcion pero no la invocacion */}
+      <Route exact path='/' render={() => <Cards
+        cities={cities}
         onClose={onClose}
-       />   }   />
-      
-       <Route exact path = '/about' render = { () => <About/> }         />
-       <Route exact path ='/ciudad/:ciudadId' render = { ({match}) => {
-        <Ciudad
-          ciudadId={match.params.ciudadId}
-          ciudad= {onFilter(match.params.ciudadId)}
-        />}}
-       />
-     
+      />} />
+
+      <Route path='/about' render={() => <About />} />
+      <Route exact path='/ciudad/:ciudadId' render={({ match }) => {
+        return (
+          <Ciudad ciudad={onFilter(match.params.ciudadId)} />
+        )
+      }}
+
+      />
+
     </div>
   );
 }
--- src/components/Ciudad.jsx
+++ src/components/Ciudad.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import estilos from "./Ciudad.module.css";
 
 
-function Ciudad ({ciudadId, ciudad, temp}) {
-     
-  let params = useParams();
-  if (!ciudad) {alert("No toquen la URL")}
-      return   (
-    
-       <div className="info">
-        <h3>Ciudad</h3>
-        <h2>{ciudad.name}</h2>
-        <div> Temperatura:{temp}</div>
-        <div>Clima: {ciudad.weather}</div>
-        <div>Viento : {ciudad.wind}</div>
-        <div>Cantidad de nubes: {ciudad.clouds}</div>
-        <div>Latitud: {ciudad.latitud}º</div>
-        <div>Longitud: {ciudad.longitud}º</div>
-       </div>
-      )
+function Ciudad({ ciudad, img }) {
+
+    let params = useParams();
+    console.log(ciudad)
+
+    if (!ciudad) { alert("No toquen la URL") }
+    else
+
+        return (
+            <div >
+
+                <div className={estilos.contenedor}>
+                    <h3>{ciudad.name}</h3>
+                    <div className={estilos.img}> <img src={`http://openweathermap.org/img/wn/${ciudad.img}@2x.png`} alt="Img del tiempo" /></div>
+                    <div className={estilos.infoCont}> Temperatura: {ciudad.temp} Cº</div>
+                    <div className={estilos.infoCont}>Clima: {ciudad.weather}</div>
+                    <div className={estilos.infoCont}>Viento : {ciudad.wind}</div>
+                    <div className={estilos.infoCont}>Cantidad de nubes: {ciudad.clouds}</div>
+                    <div className={estilos.infoCont}>Latitud: {ciudad.latitud}º</div>
+                    <div className={estilos.infoCont}>Longitud: {ciudad.longitud}º</div>
+
+                </div>
+            </div>
+        )
 }
 
 export default Ciudad;
